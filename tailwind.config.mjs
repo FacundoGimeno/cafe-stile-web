d95143f2239c9c154866a15fa8dfadc5abd2c9ec
CroissantDog <croissant_dog@protonmail.com>
2024-05-08 02:07:08 -0300

    more fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'amano' : "url('https://images.alphacoders.com/908/908775.png')",
-				'maika': "url('https://images3.alphacoders.com/962/962687.png')",
-				'kaho': "url('https://images5.alphacoders.com/962/962660.png')"
-			}
-		},
-	},
-	plugins: [
-		plugin(function({ addUtilities }) {
-			addUtilities({
-				'.no-scrollbar::-webkit-scrollbar': {
-					'display': 'none',
-				},
-				'.no-scrollbar': {
-					'-ms-overflow-style' : 'none',
-					'scrollbar-width': 'none'
-				}
-			})
-		}),
-	]
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        amano: "url('/cafestileweb/assets/backgrounds/amano.webp')",
+        maika: "url('/cafestileweb/assets/backgrounds/maika.webp')",
+        kaho: "url('/cafestileweb/assets/backgrounds/kaho.webp')",
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      });
+    }),
+  ],
+};
